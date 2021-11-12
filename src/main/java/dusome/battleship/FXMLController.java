51cfad6ed520f774ab7c9d178317a60e5334d31a
mdusome <44036272+mdusome@users.noreply.github.com>
2021-11-12 13:55:57 -0500

    switches images based on random numbers

--- src/main/java/dusome/battleship/FXMLController.java
+++ src/main/java/dusome/battleship/FXMLController.java
@@ -1,4 +1,5 @@
 package dusome.battleship;
+
 /*
 Put header here
 MAthew Dusome
@@ -7,23 +8,38 @@ MAthew Dusome
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.ThreadLocalRandom;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class FXMLController implements Initializable {
-    
+
     @FXML
-    private Label lblOut;
-    
+    private ImageView img1;
     @FXML
-    private void btnClickAction(ActionEvent event) {
-        lblOut.setText("Hello World!");
+    private ImageView img2;
+
+    int rand = ThreadLocalRandom.current().nextInt(1, 2 + 1);
+
+    @FXML
+    void imgClick(MouseEvent event) {
+        ImageView img = (ImageView) event.getSource();
+        Image king = new Image(getClass().getResource("/king.png").toString());
+        if (rand == 1 && img.getId().equals("img1")) {
+            img.setImage(king);
+        } else if (rand == 2 && img.getId().equals("img2")) {
+            img.setImage(king);
+        }
+
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 }
